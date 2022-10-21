1949a02b4ef4024dff090bec03772f8ad65e677d
Allen Chang <blue69221@gmail.com>
2022-10-21 15:28:12 +0800

    first version

---
+++ function.py
@@ -0,0 +1,55 @@
+import os # operating system
+
+def read_file(filename):
+    products = []    
+    # with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 條件符合就跳到下一回再繼續執行下面程式
+            name, price = line.strip().split(',') # .strip():移除換行、split():分割
+            products.append([name, price])
+    return products
+
+# 讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱：')
+        if name == 'q': # quit
+            break
+        price = input('請輸入商品價格：')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
+
+# 印出所有購買記錄
+def print_products(products):
+    for p in products:
+        # print(p[0])
+        print(f'商品名稱：{p[0]} 的價格為 {p[1]}')
+
+# 寫入檔案
+def write_file(filename, products):
+    # with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w') as f:
+        f.write(f'商品,價格\n')
+        for p in products:
+            # f.write(p[0] + ',' + str(p[1]) + '\n')
+            f.write(f'{p[0]},{p[1]}\n')
+
+
+def main():
+    products = []
+    filename = 'products.csv'
+    if os.path.isfile(filename): # 檢查檔案在不在
+        print('找到檔案了!')
+        products = read_file(filename)
+    else:
+        print('找不到檔案!')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
